4d098302c6b83a711311c8f0eb437de27db74f7b
johnedelmapa <johnedel.mapa@sun-asterisk.com>
2019-11-09 16:48:54 +0800

    [Function] Create Sign-up

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  before_save { email.downcase! }
+  # REGEX = Regular expression
+  # To validate correct email structure
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :name,  presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: {minimum: 6}, allow_nil: true
+end
