9c181b058aa9551a8ab6c3803d1a44c965ca76b9
johnedelmapa <johnedel.mapa@sun-asterisk.com>
2019-11-10 09:12:09 +0800

    [Function]Create login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      flash[:success] = "Successfully logged in."
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Invalid Credentials"
+      render 'new'
+    end
+  end
 end
