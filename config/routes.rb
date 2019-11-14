6cd9d52f01ce767d81e5b1dcf8c4ca5f99c145ce
johnedelmapa <johnedel.mapa@sun-asterisk.com>
2019-11-14 21:49:05 +0800

    Create Categories

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   namespace :admin do 
     get '/home', to:'users#home'
     resources :users
+    resources :categories 
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_084033) do
+ActiveRecord::Schema.define(version: 2019_11_12_165159) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
