f3236887fa5be1d8a8e23265726754e15be951a0
Johnedel Mapa <johnedel.mapa@sun-asterisk.com>
2019-11-19 01:13:49 +0800

    Create Admin Category Words

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :description,  presence: true, length: { maximum: 150 }
+  has_many :words, dependent: :destroy
 end
---
+++ app/models/word.rb
@@ -0,0 +1,26 @@
+class Word < ApplicationRecord
+  belongs_to :category
+  has_many :choices, dependent: :destroy
+  accepts_nested_attributes_for :choices
+  validates :content, presence: true, length: {maximum: 50}
+  validate :has_one_correct_answer
+  validate :has_unique_choices
+
+  def correct_answer
+    choices.find_by(correct: true).content
+  end
+
+  private
+  def has_one_correct_answer
+    if choices.select{ |choices| choices.correct? }.count != 1 
+      return errors.add :base, "Must have one correct choice"
+    end  
+  end
+
+  def has_unique_choices
+    if choices.uniq { |choice| choice.content }.count < choices.length
+      return errors.add :base, "Must have unique choices"
+    end
+  end
+end
+
