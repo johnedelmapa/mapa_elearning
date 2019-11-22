f032ec96879ba4cb3d6b6c30485bf4d136a85c7e
Johnedel Mapa <johnedel.mapa@sun-asterisk.com>
2019-11-22 17:52:28 +0800

    User Lesson Interaction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, except: [:show]
+  before_action :logged_in_user, except: [:show, :new, :create]
   before_action :correct_user, only: [:edit, :update]
 
   def index
