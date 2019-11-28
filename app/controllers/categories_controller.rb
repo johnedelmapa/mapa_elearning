5b812dfab915c8daa0fab8ee253cb5c003b110d2
Johnedel Mapa <johnedel.mapa@sun-asterisk.com>
2019-11-28 20:50:23 +0800

    Filter-lesson

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,5 +3,12 @@ class CategoriesController < ApplicationController
     @categories = Category.paginate(page: params[:page], per_page: 6)
     @lesson = Lesson.new
     @user = User.find(current_user.id)
+    @status = params[:status]
+    
+    if @status == "1"
+      @categories =  @user.categories.paginate(page: params[:page], per_page: 10)
+    elsif @status == "0"
+        @categories = @categories.where.not(id:  @user.category_ids).paginate(page: params[:page], per_page: 10)
+    end
   end
 end
